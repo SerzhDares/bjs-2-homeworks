58409aedce86186b96d6127b1fee981e0a4efc96
SerzhDares <126406118+SerzhDares@users.noreply.github.com>
2023-07-06 12:33:00 +0400

    Fifth task

---
+++ 5.classes/task.js
@@ -0,0 +1,94 @@
+class PrintEditionItem {
+    constructor(name, releaseDate, pagesCount) {
+        this.name = name;
+        this.releaseDate = releaseDate;
+        this.pagesCount = pagesCount;
+        this._state = 100;
+        this.type = null;
+    }
+
+    fix() {
+        this._state *= 1.5;
+        if (this._state > 100) {
+            this._state = 100;
+        }
+    }
+
+    set state(value) {
+        
+        if (value < 0) {
+            this._state = 0;
+            return;
+        }
+        if (value > 100) {
+            this._state = 100;
+            return;
+        }
+        this._state = value;
+    }
+
+    get state() {
+        return this._state;
+    }
+}
+
+class Magazine extends PrintEditionItem {
+    constructor(name, releaseDate, pagesCount) {
+        super(name, releaseDate, pagesCount);
+        this.type = "magazine";
+    }
+}
+
+class Book extends PrintEditionItem {
+    constructor(author, name, releaseDate, pagesCount) {
+        super(name, releaseDate, pagesCount);
+        this.type = "book";
+        this.author = author;
+    }
+}
+
+class NovelBook extends Book {
+    constructor(author, name, releaseDate, pagesCount) {
+        super(author, name, releaseDate, pagesCount);
+        this.type = "novel";
+    }
+}
+
+class FantasticBook extends Book {
+    constructor(author, name, releaseDate, pagesCount) {
+        super(author, name, releaseDate, pagesCount);
+        this.type = "fantastic";
+    }
+}
+
+class DetectiveBook extends Book {
+    constructor(author, name, releaseDate, pagesCount) {
+        super(author, name, releaseDate, pagesCount);
+        this.type = "detective";
+    }
+}
+
+class Library extends PrintEditionItem {
+    constructor(name) {
+        super(name);
+        this.books = [];
+    }
+
+    addBook(book) {
+        if (this.state > 30) {
+            this.books.push(book);
+        }
+    }
+
+    findBookBy(type, value) {
+        return this.books.find(book => book[type] === value) || null;
+    }
+
+    giveBookByName(bookName) {
+        let book = this.findBookBy('name', bookName);
+        if (book) {
+            this.books.splice(this.books.indexOf(this.book), 1);
+        }
+            return book;
+    }
+}
