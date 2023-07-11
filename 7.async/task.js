739ce6184e223d991194402f0a116732bae7d12d
SerzhDares <126406118+SerzhDares@users.noreply.github.com>
2023-07-11 19:24:17 +0400

    Seventh task corrected

--- 7.async/task.js
+++ 7.async/task.js
@@ -5,17 +5,13 @@ class AlarmClock {
     }
 
     addClock(time, callback) {
-        if (time === null || callback === null) {
+        if (time === null || !callback) {
             throw new Error('Отсутствуют обязательные аргументы');
         }
-        if (this.alarmCollection.find(timeValue => timeValue === time)) {
+        if (this.alarmCollection.find(timeValue => timeValue.time === time)) {
             console.warn('Уже присутствует звонок на это же время');
         }
-        callback(() => {
-            this.alarmCollection.push(time);
-        });
-        let obj = {callback, time, canCall: true};
-        this.alarmCollection.push(obj);
+        this.alarmCollection.push({callback, time, canCall: true});
     }
 
     removeClock(time) {
@@ -33,17 +29,16 @@ class AlarmClock {
         if (this.intervalId !== null) {
             return;
         }
-        setInterval(() => this.alarmCollection.forEach(timeValue => {
-            if (timeValue.time === this.getCurrentFormattedTime()) {   //не совсем понятен момент: почему в этой строке сравнение идет с вызовом метода,
+        this.intervalId = setInterval(() => this.alarmCollection.forEach(timeValue => {
+            if (timeValue.time === this.getCurrentFormattedTime() && timeValue.canCall === true) {
                 timeValue.canCall = false;
                 timeValue.callback();
             }
         }), 1000);
-        this.intervalId = setInterval;                                // а в этой строке присваивается значение метода?
     }
 
     stop() {
-        clearInterval();
+        clearInterval(this.intervalId);
         this.intervalId = null;
     }
 
